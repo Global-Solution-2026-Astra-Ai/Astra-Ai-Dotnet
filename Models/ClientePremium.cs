b802b821db9b327838a4114484b8dda83ca94860
Lucas Hideki <hideki.lucas0701@gmail.com>
2026-06-01 18:54:37 -0300

    feat(wip): client entity

---
+++ Models/ClientePremium.cs
@@ -0,0 +1,27 @@
+namespace AstraAiDotnet.Models
+{
+    public class ClientePremium
+    {
+        public long IdCliente { get; private set; }
+        public string RazaoSocial { get; private set; }
+        public string Cnpj { get; private set; }
+        public decimal DemandaContratadaGwh { get; private set; }
+        public string StatusCadastro { get; private set; }
+
+        public ICollection<LogTransacao> Transacoes { get; set; } = new List<LogTransacao>();
+
+        public ClientePremium() { }
+
+        public ClientePremium(string razaoSocial, string cnpj, decimal demandaContratadaGwh, string statusCadastro)
+        {
+            RazaoSocial = razaoSocial;
+            Cnpj = cnpj;
+            DemandaContratadaGwh = demandaContratadaGwh;
+            StatusCadastro = statusCadastro;
+        }
+
+        public void SetRazaoSocial(string novaRazaoSocial) => RazaoSocial = novaRazaoSocial;
+        public void SetDemanda(decimal novaDemanda) => DemandaContratadaGwh = novaDemanda;
+        public void SetStatus(string novoStatus) => StatusCadastro = novoStatus;
+    }
+}
