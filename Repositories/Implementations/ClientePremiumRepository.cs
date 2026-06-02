b2c563edb40ec19c2fe885a824228b0635f3b443
Lucas Hideki <hideki.lucas0701@gmail.com>
2026-06-01 21:19:51 -0300

    feat(wip): client repository

---
+++ Repositories/Implementations/ClientePremiumRepository.cs
@@ -0,0 +1,59 @@
+using AstraAiDotnet.Data;
+using AstraAiDotnet.Models;
+using AstraAiDotnet.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace AstraAiDotnet.Repositories.Implementations
+{
+    public class ClientePremiumRepository : IClientePremiumRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ClientePremiumRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ClientePremium>> ListarAsync()
+        {
+            return await _context.ClientesPremium.ToListAsync();
+        }
+
+        public async Task<ClientePremium?> ObterPorIdAsync(long idCliente)
+        {
+            return await _context.ClientesPremium.FindAsync(idCliente);
+        }
+
+        public async Task<ClientePremium?> ObterPorCnpjAsync(string cnpj)
+        {
+            return await _context.ClientesPremium.FirstOrDefaultAsync(c => c.Cnpj == cnpj);
+        }
+
+        public async Task AdicionarAsync(ClientePremium cliente)
+        {
+            await _context.ClientesPremium.AddAsync(cliente);
+            await SalvarAsync();
+        }
+
+        public async Task UpdateAsync(ClientePremium cliente)
+        {
+            _context.ClientesPremium.Update(cliente);
+            await SalvarAsync();
+        }
+
+        public async Task DeleteAsync(long idCliente)
+        {
+            var cliente = await _context.ClientesPremium.FindAsync(idCliente);
+            if (cliente is not null)
+            {
+                _context.ClientesPremium.Remove(cliente);
+                await SalvarAsync();
+            }
+        }
+
+        public async Task SalvarAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
