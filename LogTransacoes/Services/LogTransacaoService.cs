93c93dec530a7ae5bfc8cbf451d9bf30843df413
Lucas Hideki <hideki.lucas0701@gmail.com>
2026-06-04 18:19:18 -0300

    feat(wip): transaction saving method

---
+++ LogTransacoes/Services/LogTransacaoService.cs
@@ -0,0 +1,43 @@
+using AstraAiDotnet.LogTransacoes.Models;
+using AstraAiDotnet.LogTransacoes.Repositories.Interfaces;
+using AstraAiDotnet.LogTransacoes.DTOs;
+
+namespace AstraAiDotnet.LogTransacoes.Services
+{
+    public class LogTransacaoService
+    {
+        private readonly ILogTransacaoRepository _logTransacaoRepository;
+
+        public LogTransacaoService(ILogTransacaoRepository logTransacaoRepository)
+        {
+            _logTransacaoRepository = logTransacaoRepository;
+        }
+
+        private static LogTransacaoResponse MapToResponse(LogTransacao logTransacao)
+        {
+            return new LogTransacaoResponse
+            {
+                IdTransacao = logTransacao.IdTransacao,
+                IdLeilao = logTransacao.IdLeilao,
+                IdClienteVencedor = logTransacao.IdClienteVencedor,
+                ValorArrematado = logTransacao.ValorArrematado,
+                TaxaAstra = logTransacao.TaxaAstra,
+                DataFaturamento = logTransacao.DataFaturamento
+            };
+        }
+
+        public async Task<LogTransacaoResponse> RegistrarLogTransacaoAsync(LogTransacaoRequest logTransacaoRequest)
+        {
+            var logTransacao = new LogTransacao(
+                logTransacaoRequest.IdLeilao,
+                logTransacaoRequest.IdClienteVencedor,
+                logTransacaoRequest.ValorArrematado,
+                DateTime.Now
+            );
+
+            await _logTransacaoRepository.RegistrarLogTransacaoAsync(logTransacao);
+
+            return MapToResponse(logTransacao);
+        }
+    }
+}
