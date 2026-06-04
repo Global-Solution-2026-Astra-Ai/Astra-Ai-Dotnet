28357c9bce15696ffe9684f41b83bf85dfb8a64c
Lucas Hideki <hideki.lucas0701@gmail.com>
2026-06-04 18:24:08 -0300

    feat(wip): dependency injection on program

--- Program.cs
+++ Program.cs
@@ -6,8 +6,13 @@ using AstraAiDotnet.ClientesPremium.Repositories.Interfaces;
 using AstraAiDotnet.ClientesPremium.Repositories.Implementations;
 using AstraAiDotnet.Leiloes.Repositories.Interfaces;
 using AstraAiDotnet.Leiloes.Repositories.Implementations;
+using AstraAiDotnet.LogTransacoes.Repositories.Interfaces;
+using AstraAiDotnet.LogTransacoes.Repositories.Implementations;
+using AstraAiDotnet.LogTransacoes.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Console.WriteLine($"Environment: {builder.Environment.EnvironmentName}");
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -23,6 +28,9 @@ builder.Services.AddScoped<ClientePremiumService>();
 
 builder.Services.AddScoped<ILeilaoRepository, LeilaoRepository>();
 builder.Services.AddScoped<LeilaoService>();
+
+builder.Services.AddScoped<ILogTransacaoRepository, LogTransacaoRepository>();
+builder.Services.AddScoped<LogTransacaoService>();
 
 // Add Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
